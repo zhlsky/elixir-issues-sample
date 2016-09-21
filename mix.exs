291520c16e12c0e74b6908dad2e2ba7f019ea5c5
r_cho_koryo <zhanghl_116@r.recruit.co.jp>
2016-09-22 01:17:12 +0900

    elixir project sample.

---
+++ mix.exs
@@ -0,0 +1,47 @@
+defmodule Issues.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :issues,
+      escript: escript_config,
+      version: "0.0.1",
+      name: :"Issues",
+      source_url: "https://github.com/zhlsky/elixir-issues-sample",
+      elixir: "~> 1.3",
+      build_embedded:  Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+   ]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    [applications: [:logger, :httpoison]]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
+  defp deps do
+    [
+      httpoison: "~> 0.9.1",
+      poison: "~> 2.2.0",
+      ex_doc: "~> 0.13.2",
+      earmark: "~> 1.0.1"
+    ]
+  end
+
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
+  end
+end
